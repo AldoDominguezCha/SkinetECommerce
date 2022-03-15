6e86500eff2990f4c56dc85c5c54652907af2191
Aldo Dominguez <aldo.dominguez@thomsonreuters.com>
2022-03-15 09:52:59 -0600

    refactorize application into separate projects (Infrastructure and Core)

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
-using API.Entities;
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,9 +22,9 @@ namespace API.Controllers
         }
 
         [HttpGet("{id:int}")]
-        public string GetProduct(int id) 
+        public async Task<ActionResult<Product>> GetProduct(int id) 
         {
-            return $"single product - Your ID : {id}";
+            return Ok(await _context.Products.FindAsync(id));
         }
 
     }
