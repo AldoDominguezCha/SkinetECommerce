f0f68f67e593d723cf7a39b783541be329dc0d97
Aldo Dominguez <aldodominguezchz@gmail.com>
2022-03-30 17:11:44 -0600

    add filter by name in products with 'search' query parameter and add Cross-Origin-Resource-Sharing support

--- Core/Specifications/ProductSpecificationParams.cs
+++ Core/Specifications/ProductSpecificationParams.cs
@@ -16,9 +16,14 @@ namespace Core.Specifications
             get => _pageSize;
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value; 
         }
-
         public int? BrandId { get; set; }
         public int? TypeId { get; set; }
         public string Sort { get; set; }
+        private string _search;
+        public string Search 
+        {
+            get => _search;
+            set => _search = value.ToLower();
+        }
     }
 }
